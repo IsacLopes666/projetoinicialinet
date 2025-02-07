4b58de6b7a7a724209508698d56154fadc1534e6
ISAC LOPES HENRIQUE <i.henrique@labcetec.net>
2025-02-07 11:17:09 -0300

    ultilizando css para estilizaçao, tenho que concertar

---
+++ src/componentes/MyComponent.jsx
@@ -0,0 +1,15 @@
+import "./NewComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+
+
+      <h2>Meu componente, criado por mim mesmo</h2>
+      <p style={{color: "blue", fontSize:"40px", 
+        backgroundColor: "black"}}>Autoria: Eu</p>
+    </div>
+  )
+}
+
+export default MyComponent
---
+++ src/componentes/OtherComponent.jsx
@@ -0,0 +1,11 @@
+import styles from './Othercompenent.module.css';
+
+const OtherComponent = () => {
+  return (
+    <div>
+      <h2 className={styles.titulo}>'ACElERA' </h2>
+    </div>
+  )
+}
+
+export default OtherComponent
